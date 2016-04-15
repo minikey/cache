348a3f0b8d69450a758bd1b8b688e95ee42c57dc
fumin <fumin@inagora.cn>
2016-04-15 14:33:49 +0800

    修改缩紧

--- cache.js
+++ cache.js
@@ -1,88 +1,88 @@
 export default class Cache {
 
-	constructor(limit) {
-		this.limit = limit | 0;
-		this.size = 0;
-		this.map = Object.create(null);
-	}
+    constructor(limit) {
+        this.limit = limit | 0; 
+        this.size = 0;
+        this.map = Object.create(null);
+    }
 
-	shift() {
-		var entry = this.head;
+    shift() {
+        var entry = this.head;
 
-		if (entry) {
-			this.head = this.head.newer;
-			this.head.order = void(0);
-			entry.newer = entry.older = void(0);
-			this.map[entry.key] = void(0);
-			this.size--;
-		}
+        if (entry) {
+            this.head = this.head.newer;
+            this.head.order = void(0);
+            entry.newer = entry.older = void(0);
+            this.map[entry.key] = void(0);
+            this.size--;
+        }
 
-		return entry;
-	}
+        return entry;
+    }
 
-	get(key, returnEntry) {
-		let entry = this.map[key];
+    get(key, returnEntry) {
+        let entry = this.map[key];
 
-		if (entry === void(0)) {
-			return;
-		}
+        if (entry === void(0)) {
+            return;
+        }
 
-		if (entry === this.tail) {
-			return returnEntry ? entry : entry.value;
-		}
+        if (entry === this.tail) {
+            return returnEntry ? entry : entry.value;
+        }
 
-		if (entry.newer) {
-			if (this.head === entry.newer) {
-				this.head = entry;
-			}
+        if (entry.newer) {
+            if (this.head === entry.newer) {
+                this.head = entry;
+            }
 
-			entry.newer.older = entry.older;
-		}
+            entry.newer.older = entry.older;
+        }
 
-		if (entry.older) {
-			entry.older.newer = entry.newer;
-		}
+        if (entry.older) {
+            entry.older.newer = entry.newer;
+        }
 
-		entry.newer = void(0);
-		entry.older = this.tail;
+        entry.newer = void(0);
+        entry.older = this.tail;
 
-		if (this.tail) {
-			this.tail.newer = entry;
-		}
+        if (this.tail) {
+            this.tail.newer = entry;
+        }
 
-		this.tail = entry;
+        this.tail = entry;
 
-		return returnEntry ? entry : entry.value;
-	}
+        return returnEntry ? entry : entry.value;
+    }
 
-	put(key, val) {
-		let entry = this.get(key, true),
-			removed;
+    put(key, val) {
+        let entry = this.get(key, true),
+            removed;
 
-		if (this.size === this.limit) {
-			removed = this.shift();
-		}
+        if (this.size === this.limit) {
+            removed = this.shift();
+        }
 
-		if (!entry) {
-			entry = {
-				key: key
-			};
+        if (!entry) {
+            entry = {
+                key: key
+            };
 
-			this.map[key] = entry;
+            this.map[key] = entry;
 
-			if (this.tail) {
-				this.tail.newer = entry;
-				entry.order = this.tail;
-			} else {
-				this.head = entry;
-			}
+            if (this.tail) {
+                this.tail.newer = entry;
+                entry.order = this.tail;
+            } else {
+                this.head = entry;
+            }
 
-			this.tail = entry;
-			this.size++;
-		}
+            this.tail = entry;
+            this.size++;
+        }
 
-		entry.value = val;
+        entry.value = val;
 
-		return removed;
-	}
+        return removed;
+    }
 }
